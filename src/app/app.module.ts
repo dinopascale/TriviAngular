88f9dc728f10938b690d573f35266332714092df
dinopascale <dino.pascale@gmail.com>
2018-06-05 15:06:15 +0200

    removed unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { NewGameGuard } from './shared/new-game-guard.service';
-import { EffectsModule } from '@ngrx/effects';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { CoreModule } from './core/core.module';
 import { environment } from './../environments/environment';
 
-import { CoreModule } from './core/core.module';
-import { ResultsModule } from './results/results.module';
 
 import { HomeEffects } from './core/home/store/home.effects';
 import { reducers, metaReducers } from './store/app.reducers';
